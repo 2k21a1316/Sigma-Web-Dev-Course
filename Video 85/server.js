7b22f34298937ba2c417eeee00cff4aee5f6bde9
Vishal <97728453+2k21a1316@users.noreply.github.com>
2024-06-16 19:05:48 +0000

    done

--- Video 85/server.js
+++ Video 85/server.js
@@ -3,6 +3,7 @@ var slugify = require('slugify')
 let a = slugify('some string') // some-string
 console.log(a)
 
+// slugify convert string into slug -> mean string without space
 // if you prefer something other than '-' as separator
 const b = slugify('some st&&*(^%$$^^&ring', '_')  // some_string
 console.log(b)
