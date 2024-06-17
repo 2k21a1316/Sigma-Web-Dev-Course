9512ed366fbefd5f8688231241e69b46fe62a749
Vishal <97728453+2k21a1316@users.noreply.github.com>
2024-06-17 07:39:36 +0000

    done

--- Video 87/main.js
+++ Video 87/main.js
@@ -1,14 +1,16 @@
-const fs = require("fs")
+const fs = require("fs")//npm init -y always need to inititalise node project
 // const fs = require("fs/promises")
  
 console.log("starting")
 // fs.writeFileSync("harry.txt", "Harry is a good boy")
+//js run on asynchronous approach
 
 fs.writeFile("harry2.txt", "Harry is a good boy2", ()=>{
     console.log("done")
-    fs.readFile("harry2.txt", (error, data)=>{
+    fs.readFile("harry2.txt", (error, data)=>{//shceduled
         console.log(error, data.toString())
     })
+    // creates callnack hell if may code like above code ,nesting
 })
 
 fs.appendFile("harry.txt", "harryrobo", (e, d)=>{
--- Video 87/mainpromise.js
+++ Video 87/mainpromise.js
@@ -1,6 +1,6 @@
 import fs from "fs/promises"
-
+//promise to avoid callback hell
 let a = await fs.readFile("harry.txt")
-
+// no need of asyncs for await ,can do directly await ->wait for
 let b = await fs.appendFile("harry.txt", "\n\n\n\nthis is amazing promise")
 console.log(a.toString(), b)
