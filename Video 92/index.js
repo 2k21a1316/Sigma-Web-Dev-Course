1228ecb5fef87a0735e99a895731f5048492956a
Vishal <97728453+2k21a1316@users.noreply.github.com>
2024-06-17 13:58:06 +0000

    done

--- Video 92/index.js
+++ Video 92/index.js
@@ -5,17 +5,17 @@ const port = 3000
 app.set('view engine', 'ejs');
 
 // https://github.com/mde/ejs/wiki/Using-EJS-with-Express
-
+//template engine is ejs ,pug,template engine handlebars
 app.get('/', (req, res) => {
     let siteName = "Adidas"
     let searchText = "Search Now"
     let arr = ["Hey", 54, 65]
-    res.render("index", { siteName: siteName, searchText: searchText, arr })
-})
+    res.render("index", { siteName: siteName, searchText: searchText, arr })//use .render not .sendfile for ejs template engine
+})//<%= siteName %> gives value of sitename
 
 app.get('/blog/:slug', (req, res) => {
     let blogTitle = "Adidas why and when?"
-    let blogContent = "Its a very good brand"
+    let blogContent = "Its a very good brand"//hum in variables ko is template me ghusana chahte hai,so use ejs ->template engine
     res.render("blogpost", {blogTitle: blogTitle, blogContent: blogContent})
 })
 
