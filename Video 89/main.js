4644cba1674f53be052a18b626750f6a1df222de
Vishal <97728453+2k21a1316@users.noreply.github.com>
2024-06-17 11:41:24 +0000

    done

--- Video 89/main.js
+++ Video 89/main.js
@@ -2,24 +2,25 @@ const express = require('express')
 const blog = require('./routes/blog')
 const shop = require('./routes/shop')
  
-
-
+// browser for client and console is for server 
+// sensitive info by post request 
 const app = express()
 const port = 3000
+
 
 app.use(express.static("public"))
 app.use('/blog', blog)
 app.use('/shop', shop)
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => {//this request can check by browser
     console.log("Hey its a get request")
     res.send('Hello World21!')
-}).post('/', (req, res) => {
+}).post('/', (req, res) => {//chained with above function
     console.log("Hey its a post request")
     res.send('Hello World post!')
 })
 
-app.put('/', (req, res) => {
+app.put('/', (req, res) => {//this request used for to update
     console.log("Hey its a put request")
     res.send('Hello World put!')
 })
